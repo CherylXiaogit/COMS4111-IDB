43ca8c24f95b70322e143407c28c7716cdb2312c
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-03-06 12:28:39 -0500

    Add region sql

---
+++ Data_Population/sql/region.sql
@@ -0,0 +1,11 @@
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (1, 15106, point(-80.09,40.38), point(-80.09,40.38));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (2, 15202, point(-80.05,40.5), point(-80.05,40.5));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (3, 15232, point(-79.93,40.45), point(-79.93,40.45));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (4, 28202, point(-80.84,35.22), point(-80.84,35.22));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (5, 28273, point(-80.88,35.24), point(-80.88,35.24));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (6, 53562, point(-89.52,53.09), point(-89.52,53.09));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (7, 29708, point(-80.98,35.06), point(-80.98,35.06));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (8, 61820, point(-88.24,40.11), point(-88.24,40.11));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (9, 85004, point(-112.07,33.45), point(-112.07,33.45));
+INSERT INTO Region (Region_id, Zip_code, NW_point, SE_point) VALUES (10, 89014, point(-115.04,35.06), point(-115.04,35.06));
+
