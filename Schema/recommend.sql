20a93fb7c166391ff9c1bb081b44f2f83ba420aa
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-03-06 12:52:38 -0500

    Add recommend sql

---
+++ Schema/recommend.sql
@@ -0,0 +1,11 @@
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (1, 5);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (3, 5);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (4, 12);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (5, 13);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (8, 14);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (1, 1692);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (3, 1698);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (3, 1697);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (5, 737);
+INSERT INTO Recommend (Feature_id, Restaurant_id) VALUES (4, 738);
+
